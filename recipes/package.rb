46311b7ade146852cb72fc4460f87fa485df3626
Thomas Boerger <tboerger@suse.de>
2015-03-17 11:46:09 +0100

    Refresh the repos after added

--- recipes/package.rb
+++ recipes/package.rb
@@ -26,7 +26,7 @@ when "suse"
     key node["packer"]["zypper"]["key"]
     title node["packer"]["zypper"]["title"]
 
-    action :add
+    action [:add, :refresh]
 
     only_if do
       node["packer"]["zypper"]["enabled"]
